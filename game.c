f7849fa79f5d78239a698c8769dfd32fe99d952d
zsqrvs <shafalkon@gmail.com>
2023-02-27 22:20:01 +0300

    make 3 files (main, utils, header)

---
+++ game.c
@@ -0,0 +1,54 @@
+//
+// Created by shafa on 27.02.2023.
+//
+
+#include "game.h"
+
+void print_frame(Game *game) {
+    for (int i = 0; i < SIZE_Y; i++) {
+        for (int j = 0; j < SIZE_X; j++) {
+            printf("%c", game->screen[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void draw_playground(char screen[SIZE_Y][SIZE_X]) {
+    for (int i = 0; i < SIZE_Y; i++) {
+        for (int j = 0; j < SIZE_X; j++) {
+            screen[i][j] = ((i == 0) || (i == SIZE_Y - 1)) || ((j == 0) || (j == SIZE_X - 1)) ? '#' : ' ';
+        }
+    }
+}
+
+Unit initInvader() {
+    FILE *file = fopen("invader.txt", "r");
+
+    Unit invader;
+    fscanf(file, "%i%i", &invader.sizeY, &invader.sizeX);
+
+    invader.texture = (char **) malloc(invader.sizeY * sizeof(char *));
+
+    for (int i = 0; i < invader.sizeY; i++) {
+        invader.texture[i] = (char *) malloc(invader.sizeX);
+        fscanf(file, "%s", invader.texture[i]);
+    }
+
+    fclose(file);
+}
+
+Unit initPlayer(){
+    FILE *file = fopen("player.txt", "r");
+
+    Unit player;
+    fscanf(file, "%i%i", &player.sizeY, &player.sizeX);
+
+    player.texture = (char**)malloc(player.sizeY * sizeof(char*));
+
+    for (int i = 0; i < player.sizeY; i++) {
+        player.texture[i] = (char*) malloc(player.sizeX);
+        fscanf(file, "%s", player.texture[i]);
+    }
+
+    fclose(file);
+}
---
+++ game.h
@@ -0,0 +1,38 @@
+//
+// Created by shafa on 27.02.2023.
+//
+
+#ifndef SPACEINVADERS_GAME_H
+#define SPACEINVADERS_GAME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FPS 5
+#define SIZE_X 256
+#define SIZE_Y 256
+
+
+typedef struct unit {
+    int x, y;
+    signed char state;
+    char **texture;
+    int sizeX, sizeY;
+} Unit;
+
+typedef struct game {
+    char screen[SIZE_Y][SIZE_X];
+    Unit player, *invader, *bullet;
+    unsigned scoreCounter;
+} Game;
+
+void print_frame(Game *game);
+void draw_playground(char screen[SIZE_Y][SIZE_X]);
+Unit initInvader();
+Unit initPlayer();
+
+#endif //SPACEINVADERS_GAME_H
+
+
+
+
