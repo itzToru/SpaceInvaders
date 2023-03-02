177b6efb42482b6dea83f5e24e1892a9c7e7cdc8
itztoru <itztoru@vk.com>
2023-03-02 20:32:00 +0300

    Toru: isColide function prototype

--- game.h
+++ game.h
@@ -5,13 +5,14 @@
 #ifndef SPACEINVADERS_GAME_H
 #define SPACEINVADERS_GAME_H
 
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define FPS 5
 #define SIZE_X 256
 #define SIZE_Y 256
-
 
 typedef struct unit {
     int x, y;
@@ -34,10 +35,8 @@ Unit initInvader();
 
 Unit initPlayer();
 
-void draw(Unit unit);
+void draw(Unit, Game);
+
+bool isColide(Unit, Unit);
 
 #endif //SPACEINVADERS_GAME_H
-
-
-
-
