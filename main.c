e51d5b004b7013179504a4e1279b2cf1bb47969f
zsqrvs <shafalkon@gmail.com>
2023-03-08 13:40:27 +0300

    edit main

--- main.c
+++ main.c
@@ -1,12 +1,30 @@
 #include "game.h"
 
 int main() {
-    Game a;
-    while (1) {
-        draw_playground(a.screen);
-        system("cls");
-        print_frame(&a);
+    Game game;
+    Unit* invaders = malloc(3 * sizeof (Unit));
+    Unit* bullet = malloc(sizeof(Unit));
+
+    Unit player = initPlayer();
+    for(int i = 0; i < 3; i++){
+        invaders[i] = initInvader();
     }
+
+    invaders[0].y = 10;
+    invaders[0].x = 10;
+
+    invaders[1].y = 10;
+    invaders[1].x = 30;
+
+    invaders[2].y = 10;
+    invaders[2].x = 50;
+
+    draw_playground(game.screen);
+
+    for(int i = 0; i < 3; i++){
+        draw(invaders[i], game);
+    }
+
+
 }
 
-//TODO: Сделать функцию отрисовки объектов
